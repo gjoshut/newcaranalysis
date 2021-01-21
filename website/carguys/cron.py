9799b6d6bdacda580396525ab3da37d1e860ce86
Josh Garner <joshgarner@joshs-air.attlocal.net>
2021-01-21 12:19:39 -0600

    cronjob.py modification

--- website/carguys/cron.py
+++ website/carguys/cron.py
@@ -1,6 +1,7 @@
 from .scripts import database_update, to_database_presale, to_database_postsale, add_new_to_database
 import pandas as pd
 import io
+from datetime import timedelta, datetime, date
 from .models import Postsales, Presale
 from django_pandas.io import read_frame
 import os, requests, lxml.html
